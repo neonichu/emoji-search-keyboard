3534f1b2b903b2740258dded8359c1bf9f096cdd
Boris Bügling <boris@icculus.org>
2014-10-26 17:59:07 +0000

    Added IRFEmojiCheatSheet as a Pod.

---
+++ Keyboard/BridgingHeader.h
@@ -0,0 +1,9 @@
+//
+//  BridgingHeader.h
+//  TastyImitationKeyboard
+//
+//  Created by Boris Bügling on 25/10/14.
+//  Copyright (c) 2014 Apple. All rights reserved.
+//
+
+#import <IRFEmojiCheatSheet/IRFEmojiCheatSheet.h>
---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '8.0'
+
+source 'https://github.com/CocoaPods/Specs.git'
+
+target 'Keyboard' do
+	pod 'IRFEmojiCheatSheet', :git => 'git@github.com:neonichu/IRFEmojiCheatSheet.git'
+end
+
